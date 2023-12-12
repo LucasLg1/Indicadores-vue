a54dc6c27514c931e4f8aceb22c9c22da136c8f0
LucasLg1 <150838757+LucasLg1@users.noreply.github.com>
2023-12-12 09:53:57 -0300

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Dados from '../views/DadosView.vue'
 import Graficos from '../views/GraficosView.vue'
-import Teste from '../views/TesteView.vue'
 
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Graficos',
+    component: Graficos
   },
-  {
-  path: '/dados',
-  name: 'Dados',
-  component: Dados
-},
-{
-  path: '/graficos',
-  name: 'Graficos',
-  component: Graficos
-},
-{
-  path: '/teste',
-  name: 'Teste',
-  component: Teste
-},
-
-
 ]
 
 const router = createRouter({
